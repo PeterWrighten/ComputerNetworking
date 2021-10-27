a0d326de5d32035778a750ab8bf16842afd610d7
PeterWrighten <peterwrighten@gmail.com>
2021-10-27 12:34:35 +0900

    update

---
+++ prerequisites/list.py
@@ -0,0 +1,12 @@
+x = ['java', 'python', 'C', 'C++']
+x.insert(2, 'Go')
+x.append('T')
+x.pop()
+x.pop(0)
+print(x)
+
+tuple = ('ssh', 'rsa', 'hash')
+print(tuple)
+
+for i in x:
+    print(i)
---
+++ prerequisites/str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+x = 'Hi, %s, you should pay $%d' % ('Michale', 100)
+
+print(x)
