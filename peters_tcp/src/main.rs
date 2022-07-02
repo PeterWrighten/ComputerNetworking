c86ebdc0328f717b4bd6c0c0eb8fb6e435ccad18
PeterWrighten <peterwrighten@gmail.com>
2022-07-02 17:02:18 +0900

    Update: tcp-rs

--- peters_tcp/src/main.rs
+++ peters_tcp/src/main.rs
@@ -1,5 +1,10 @@
 extern crate tun_tap;
+use std::io;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes : {:x}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
